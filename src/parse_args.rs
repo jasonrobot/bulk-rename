cbf2a0d9af7530532d2e9d11eac01845bbb82e7a
Jason Howell <jasonrobot64@gmail.com>
2018-07-16 23:38:41 -0700

    I'm losing my mind, rust

--- src/parse_args.rs
+++ src/parse_args.rs
@@ -1,9 +1,11 @@
 use std::fmt;
+use std::str::FromStr;
+use std::error::Error;
 
 #[derive(Debug)]
 enum RenamerName {
-    INSERT,
-    REMOVE,
+    Insert,
+    Remove,
 }
 
 impl fmt::Display for RenamerName {
@@ -12,13 +14,35 @@ impl fmt::Display for RenamerName {
     }
 }
 
-const RENAMER_NAMES : Vec<&str> = vec![
-    "insert",
-    "remove",
-];
+#[derive(Debug)]
+struct NoRenamerError {
+    message: &'static str
+}
 
-fn get_renamer() ->  RenamerName {
-    
+impl FromStr for RenamerName {
+    type Err = NoRenamerError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "insert" => Ok(RenamerName::Insert),
+            "remove" => Ok(RenamerName::Remove),
+            _ => Err(Self::Err{message: "no thing"}),
+        }
+    }
+}
+
+fn is_str_renamer(a: &str) -> bool {
+    match RenamerName::from_str( a ) {
+        Ok(renamer) => true,
+        Err(_) => false
+    }    
+}
+
+fn is_renamer_present(args: Vec<&str>) -> Option<&&str> {
+    args.iter().find( |a| is_str_renamer(a) )
+}
+
+fn get_renamer(args: Vec<&str>) -> RenamerName {
+    RenamerName::Insert
 }
 
 #[cfg(test)]
@@ -27,7 +51,7 @@ mod tests {
 
     #[test]
     fn test_get_renamer() {
-        let argv = vec!["foo", "bar", "baz", "insert", "qwe", "asd", "zxc"];
-        let result = get_renamer();
+        let argv: Vec<&str> = vec!["foo", "bar", "baz", "insert", "qwe", "asd", "zxc"];
+        let result = get_renamer(argv);
     }
 }
